78b02e29db8c47d5a7ebc1e6409f7360ab8d3744
bambache@gmail.com <ebambach@users.noreply.github.com>
2017-10-23 16:06:04 -0400

    initial simple model for Document

---
+++ ams-ng/src/app/core/models/Document.ts
@@ -0,0 +1,18 @@
+export class Document {
+
+	Id: number;
+	Name: string;
+	Type: string;	
+	
+	constructor(
+		Id: number,
+		Name: string,
+		Type: string
+		
+	) {
+		this.Id = Id;
+		this.Name = Name;
+		this.Type = Type;
+		
+	}
+}
