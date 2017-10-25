e6534c2b2c4563777b9eb8a7971f6680efaa60e8
bambache@gmail.com <ebambach@users.noreply.github.com>
2017-10-25 15:43:43 -0400

    removed imports from code.module

--- ams-ng/src/app/core/core.module.ts
+++ ams-ng/src/app/core/core.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AssetSearchComponent } from './asset/asset-search/asset-search.component';
-import { AssetSearchListComponent } from './asset/asset-search-list/asset-search-list.component';
-
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [
-  AssetSearchComponent,
-  AssetSearchListComponent],
+  declarations: [],
 })
 export class CoreModule { }
