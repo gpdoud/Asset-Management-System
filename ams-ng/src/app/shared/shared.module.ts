ba2cc3e583480804d3ffaddecf0c7964b18e55d5
ebambach <31412533+ebambach@users.noreply.github.com>
2017-10-26 14:44:46 -0400

    empty shared.module file

--- ams-ng/src/app/shared/shared.module.ts
+++ ams-ng/src/app/shared/shared.module.ts
@@ -1,16 +1,1 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-@NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [],
-  providers: [
-    
-    
-  ],
-})
-
-
-export class SharedModule { }
