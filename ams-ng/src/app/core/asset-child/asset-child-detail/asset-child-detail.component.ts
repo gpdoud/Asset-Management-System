e71f83ff3a8d948abd973a2ed06d389f3123b202
Eric S <ydrive65@yahoo.com>
2017-10-30 14:52:30 -0400

    asset-child html

--- ams-ng/src/app/core/asset-child/asset-child-detail/asset-child-detail.component.ts
+++ ams-ng/src/app/core/asset-child/asset-child-detail/asset-child-detail.component.ts
@@ -1,6 +1,12 @@
+// Angular Core
 import { Component, Input, OnInit } from '@angular/core';
 
+// Models
 import { Asset } from '../../../shared/models/Asset';
+import { Location } from '../../../shared/models/Location';
+
+// Services
+import { LocationService } from '../../../shared/services/location.service';
 
 @Component({
   selector: 'app-asset-child-detail',
@@ -9,11 +15,24 @@ import { Asset } from '../../../shared/models/Asset';
 })
 export class AssetChildDetailComponent implements OnInit {
 
+  // asset received as input from parent
   @Input() asset: Asset;
 
-  constructor() { }
+  // list of locations to populate drop-down
+  locations: Location[];
+
+  // get a list of locations from the server
+  getLocations(): void
+  {
+    this.locSvc.list().then(resp => this.locations = resp);
+  }
+
+  // constructor and injections
+  constructor(private locSvc: LocationService) { }
 
   ngOnInit() {
+    // populate the drop-down
+    this.getLocations();
   }
 
 }
